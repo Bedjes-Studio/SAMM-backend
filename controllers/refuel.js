fd8c94a11453f752da0d49baaedaca80b8341f71
Hugo Langlais <bedjestudio@gmail.com>
2022-11-27 20:22:38 -0500

    Creation and deletion on refuel

--- controllers/refuel.js
+++ controllers/refuel.js
@@ -1,9 +1,49 @@
-// const User = require('../models/user');
-// const jwt = require('jsonwebtoken');
-// const bcrypt = require('bcrypt');
-// const config = require('../config');
-
+const Refuel = require('../models/refuel');
 
 exports.test = (req, res, next) => {
     res.status(200).json({ "Test": "Ok" });
+};
+
+exports.create = (req, res, next) => {
+    const refuel = new Refuel({
+        date: Date.now(),
+        mileage: req.body.mileage,
+        creatorId: req.auth.userId,
+        fuelType: req.body.fuelType,
+        litterPrice:  req.body.litterPrice,
+        totalCost:  req.body.totalCost,
+        litter:  req.body.litter,
+    });
+
+    refuel.save().then(
+        () => {
+            res.status(201).json({
+                message: 'Refuel saved successfully!',
+                refuel: refuel
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                "error": error
+            });
+        }
+    );
+};
+
+
+exports.delete = (req, res, next) => {
+    Refuel.deleteOne({ _id: req.body.id }).then(
+        (result) => {
+            res.status(200).json({
+                message: "Refuel deleted : " + result.deletedCount
+            });
+        }
+    ).catch(
+        (error) => {
+            res.status(400).json({
+                error: error
+            });
+        }
+    );
 };
--- routes/refuel.js
+++ routes/refuel.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const refuelCtrl = require('../controllers/refuel');
 
 router.get('/test', refuelCtrl.test);
-// router.post('/create', refuelCtrl.create);
+router.post('/create', auth, refuelCtrl.create);
 // router.update('/read', refuelCtrl.signup);
 // router.update('/update', refuelCtrl.signup);
-// router.post('/delete', refuelCtrl.signup);
+router.delete('/delete', refuelCtrl.delete);
 
 module.exports = router;
