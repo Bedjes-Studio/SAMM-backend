56efb946249518c47f6d679f624e845873d79377
Hugo Langlais <bedjestudio@gmail.com>
2022-12-11 23:18:33 -0500

    Fix production configuration loading

--- config.js
+++ config.js
@@ -11,20 +11,20 @@ const dev = {
     mongodb: {
         host: process.env.DEV_MONGODB_HOST || 'mongodb://127.0.0.1',
         port: parseInt(process.env.DEV_MONGODB_PORT) || 27017,
-        name: process.env.DEV_MONGODB_NAME || 'nodejs'
+        name: process.env.DEV_MONGODB_NAME || 'dev'
     }
 };
 
 const prod = {
-    app: {
+    api: {
         port: parseInt(process.env.PROD_API_PORT) || 3000,
         key: parseInt(process.env.PROD_API_KEY) || "azerty",
         tokenDuration: parseInt(process.env.PROD_API_TOKEN_DURATION) || "24h"
     },
     mongodb: {
-        host: process.env.TEST_DB_HOST || '127.0.0.1',
-        port: parseInt(process.env.TEST_DB_PORT) || 27017,
-        name: process.env.TEST_DB_NAME || 'test'
+        host: process.env.PROD_MONGODB_HOST || '127.0.0.1',
+        port: parseInt(process.env.PROD_MONGODB_PORT) || 27017,
+        name: process.env.PROD_MONGODB_NAME || 'prod'
     }
 };
 
@@ -33,6 +33,6 @@ const config = {
     prod
 };
 
-console.log("Environment : " + env);
+console.log(config[env]);
 
 module.exports = config[env];
