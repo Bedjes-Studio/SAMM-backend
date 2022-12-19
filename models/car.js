01f7a47f2eb0bb4d0dc322bb3aaac9aebda25a1c
Hugo Langlais <bedjestudio@gmail.com>
2022-12-18 19:20:18 -0500

    Add needed fields in car model

--- models/car.js
+++ models/car.js
@@ -5,6 +5,12 @@ const carSchema = mongoose.Schema({
     year: { type: Number, required: true },
     ownerId: { type: String, required: true },
     guestsId: { type: [String], required: true },
+    fuelType: { type: String, required: true },
+    fuelCapacity: { type: Number, required: true },
+    type: { type: String, required: true },
+    brand: { type: String, required: true },
+    model: { type: String, required: true },
+    name: { type: String, required: true },
     // TODO : add specs 
     // specsId: { type: String, required: true },
 });
